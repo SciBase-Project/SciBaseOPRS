a581dbb666f813a8cb2e7ded5800476a584697a4
Your Name <vmsi@gmail.com>
2016-11-15 16:45:44 +0530

    Fetching and updated ORCID data

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var passport = require("passport");
+var request = require('request');
+var https = require('https');
+var dotenv = require("dotenv");
+var data = {};
+var data1 = {};
+var bio = '';
+var researcher_urls=[];
+var keywords = [];
+var contact_details={};
+var affiliations_list = [];
+var works_list = [];
+
+dotenv.config()
 
 router.get('/orcid/login',
   passport.authenticate('oauth2'));
